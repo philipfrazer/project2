7743f5a78ef965e8a1a92e80802c9b304f8f5ae0
philip frazer <philipfrazer@gmail.com>
2017-02-14 14:09:28 -0500

    Updated DB connection code to use MongoLab when in Production

--- app.js
+++ app.js
@@ -18,7 +18,23 @@ var results = require('./routes/results');
 var search  = require('./routes/search');
 var home    = require('./routes/home');
 
-mongoose.connect('mongodb://localhost/movies-app');
+// mongoose.connect('mongodb://localhost/movies-app');
+
+// Connect to database
+if (process.env.MONGODB_URI) {
+  mongoose.connect(process.env.MONGODB_URI);
+}
+else {
+  mongoose.connect('mongodb://localhost/movies-app');
+}
+mongoose.connection.on('error', function(err) {
+  console.error('MongoDB connection error: ' + err);
+  process.exit(-1);
+  }
+);
+mongoose.connection.once('open', function() {
+  console.log("Mongoose has connected to MongoDB!");
+});
 
 var app = express();
 // view engine setup
