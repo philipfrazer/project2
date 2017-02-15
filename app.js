1cb38dffe95808cee441d2cfd2bafa882b72f284
philip frazer <philipfrazer@gmail.com>
2017-02-14 19:54:20 -0500

    took out all favicon stuff

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express               = require('express');
 var path                  = require('path');
-var favicon               = require('serve-favicon');
 var logger                = require('morgan');
 var cookieParser          = require('cookie-parser');
 var bodyParser            = require('body-parser');
@@ -54,8 +53,7 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// favicon use went here
 app.use(methodOverride('_method'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
